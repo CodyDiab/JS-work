808d0ab04edf69cb8263581aeb92b30c576b9078
Diab <emailcodydiab@gmail.com>
2020-10-05 09:12:23 -0500

    add event listeners

--- Exercise Files/07_03/JS/script.js
+++ Exercise Files/07_03/JS/script.js
@@ -1,1 +1,16 @@
 
+const CTA = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
+
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
+
+function reveal(e) {
+    e.preventDefault();
+    CTA.classList.toggle("hide");
+    ALERT.classList.toggle("hide");
+
+};
+
+
+CTA.addEventListener("click", reveal, false);
