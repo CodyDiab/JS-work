56c3e95173d41cbc14a59511fcc945f175592add
Diab <emailcodydiab@gmail.com>
2020-10-27 09:04:41 -0500

    function component

--- React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,23 +1,83 @@
-import React from "react";
-import ReactDOM from 'react-dom'
+import React,{Component} from "react";
+import ReactDOM,{render} from 'react-dom'
 
+let jobData = {
+  total: 50,
+  respond: 20,
+  noResponse: 10,
+  goal: 100
+
+
+}
+// class JobCounter extends Component {
+//   getPercent = decimal => {
+//     return decimal * 100 + '%'
+//   }
+//   calcGoalProgress = (total, goal) => {
+//     return this.getPercent(total/goal)
+//   }
+//   render() {
+//     return(
+//       <section>
+//         Jobs<div>
+//           <p> Total: {this.props.total}
+//           </p>
+//         </div>
+//         <div>
+//           <p>
+//             Responded: { this.props.respond}
+//           </p>
+//         </div>
+//         <div>
+//            No Response: {this.props.none}
+//         </div>
+//         <div>
+//           <p> Goal: {this.calcGoalProgress(this.props.total, this.props.goal)}</p>
+//         </div>
+//       </section>
+//     )
+//   }
+// }
 var style = {
   backgroundColor: 'orange',
   color: 'white',
   fontFamily:'arial'
 }
-class Message extends React.Component {
-   render() {
-     return(
-       <div style={style}>
-         <h1>Hello Everyone</h1>
-       </div>
-     )
-   }
+const getPercent = decimal => {
+  return decimal * 100 + '%'
+}
+const calcGoalProgress = (total, goal) => {
+  return getPercent(total/goal)
+}
+const JobCounter = (props) => {
+  
+  return(
+    <section style={style}>
+        Jobs<div>
+          <p> Total: {props.total}
+          </p>
+        </div>
+        <div>
+          <p>
+            Responded: { props.respond}
+          </p>
+        </div>
+        <div>
+           No Response: {props.none}
+        </div>
+        <div>
+          <p> Goal: {calcGoalProgress(props.total, props.goal)}</p>
+        </div>
+      </section>
+  )
 }
 
-ReactDOM.render(
- <Message/>,
+render(
+ <JobCounter 
+  total={jobData.total}
+ respond={jobData.respond}
+ none={jobData.noResponse}
+ goal={jobData.goal}/>,
 
   document.getElementById('root')
 )
