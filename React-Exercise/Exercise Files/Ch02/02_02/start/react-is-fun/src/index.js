954a4c1025b7d5132f20700b1f349b17af3a7362
Diab <emailcodydiab@gmail.com>
2020-10-29 08:55:26 -0500

    onClick toggle state

--- React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -20,16 +20,37 @@ const Book = ({title,author,pages}) => {
   )
 }
 
-const Library = ({books}) => {
-  return(<div>
-   {books.map(
-     (book, i)=><Book 
-     key={i}
-     title={book.title}
-      author={book.author}
-      pages={book.pages}/>
+class Library extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true
+    }
+      this.toggleOpenClosed = this.toggleOpenClosed.bind(this)
+    }
+    toggleOpenClosed() {
+      this.setState( previousState =>({
+        open: !previousState.open
+      }))
+    }
+  
+  render() {
+        console.log(this.state)
+        const {books} = this.props
+      return(
+      <div>
+        <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
+        
+   <button onClick={this.toggleOpenClosed}>Change</button>
+      {books.map(
+        (book, i)=><Book 
+        key={i}
+        title={book.title}
+          author={book.author}
+          pages={book.pages}/>
    )}
   </div>)
+}
 }
 render(
   <>
