79a9245215f00a7320b96d5d180c256c82225e25
Diab <emailcodydiab@gmail.com>
2020-10-29 08:41:26 -0500

    render child components by data.length

--- React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,83 +1,40 @@
 import React,{Component} from "react";
 import ReactDOM,{render} from 'react-dom'
 
-let jobData = {
-  total: 50,
-  respond: 20,
-  noResponse: 10,
-  goal: 100
 
-
-}
-// class JobCounter extends Component {
-//   getPercent = decimal => {
-//     return decimal * 100 + '%'
-//   }
-//   calcGoalProgress = (total, goal) => {
-//     return this.getPercent(total/goal)
-//   }
-//   render() {
-//     return(
-//       <section>
-//         Jobs<div>
-//           <p> Total: {this.props.total}
-//           </p>
-//         </div>
-//         <div>
-//           <p>
-//             Responded: { this.props.respond}
-//           </p>
-//         </div>
-//         <div>
-//            No Response: {this.props.none}
-//         </div>
-//         <div>
-//           <p> Goal: {this.calcGoalProgress(this.props.total, this.props.goal)}</p>
-//         </div>
-//       </section>
-//     )
-//   }
-// }
-var style = {
-  backgroundColor: 'orange',
-  color: 'white',
-  fontFamily:'arial'
-}
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-const JobCounter = (props) => {
+let bookList = [
+  {"title":"The Picture of Dorian Grey", "author":"Oscar Wilde", "pages": 346},
+  {"title": "The Picture of Dorian Grey", "author": "Oscar Wilde", "pages": 346},
+  {"title": "The Picture of Dorian Grey", "author": "Oscar Wilde", "pages": 346},
+  {"title": "The Willows", "author":"Algernon Blackwood", "pages": 36}
   
+
+]
+const Book = ({title,author,pages}) => {
   return(
-    <section style={style}>
-        Jobs<div>
-          <p> Total: {props.total}
-          </p>
-        </div>
-        <div>
-          <p>
-            Responded: { props.respond}
-          </p>
-        </div>
-        <div>
-           No Response: {props.none}
-        </div>
-        <div>
-          <p> Goal: {calcGoalProgress(props.total, props.goal)}</p>
-        </div>
-      </section>
+    <section>
+       <h2>{title}</h2>
+       <p>by: {author}</p>
+       <p>Pages: {pages} pages</p>
+    </section>
   )
 }
 
+const Library = ({books}) => {
+  return(<div>
+   {books.map(
+     (book, i)=><Book 
+     key={i}
+     title={book.title}
+      author={book.author}
+      pages={book.pages}/>
+   )}
+  </div>)
+}
 render(
- <JobCounter 
-  total={jobData.total}
- respond={jobData.respond}
- none={jobData.noResponse}
- goal={jobData.goal}/>,
+  <>
 
-  document.getElementById('root')
+<Library books={bookList}/>
+</>,
+document.getElementById('root')
 )
