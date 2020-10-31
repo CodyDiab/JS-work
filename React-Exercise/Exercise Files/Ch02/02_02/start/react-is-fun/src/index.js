65bf544c4d3e439ccea870874988c49c9b0260cb
Diab <emailcodydiab@gmail.com>
2020-10-31 07:18:06 -0500

    set default prop value

--- React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
+++ React-Exercise/Exercise Files/Ch02/02_02/start/react-is-fun/src/index.js
@@ -31,10 +31,29 @@ const NotHiring = () =>
   <p>Not hiring</p>
 </div>
 class Library extends React.Component {
+
+  static defaultProps = {
+    books: [
+      {title: "Our Lady of the Flowers", "author": "Gene Genet", "pages": 1263}
+    ]
+  }
   state = {
     open:true,
   freeBookmark: true,
-  hiring : false}
+  hiring : false,
+  data: [],
+loading:false}
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+    .then(data => data.json())
+    .then(data => this.setState({data, loading: false}))
+  }
+
+  componentDidUpdate() {
+    console.log("Updated")
+  }
 
     toggleOpenClosed = () =>  {
       this.setState( previousState =>({
@@ -48,6 +67,19 @@ class Library extends React.Component {
       return(
       <div>
         {this.state.hiring ? <Hiring/> : <NotHiring/>}
+        {this.state.loading 
+        ? 'loading...'
+        : <div>
+            {this.state.data.map(product => {
+              return (
+                <div>
+                  <h3>Library Product of the Week!</h3>
+                  <h4>{product.name}</h4>
+                  <img src={product.image} height={100} alt={product.name}/>
+                </div>
+              )
+            })}
+         </div>}
         <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
         
    <button onClick={this.toggleOpenClosed}>Change</button>
