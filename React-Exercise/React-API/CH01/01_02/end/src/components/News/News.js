dd9f523f61849ee50ad1b03eb543bf4bec8bf260
Diab <emailcodydiab@gmail.com>
2020-11-24 13:26:12 -0600

    init api call

--- React-Exercise/React-API/CH01/01_02/end/src/components/News/News.js
+++ React-Exercise/React-API/CH01/01_02/end/src/components/News/News.js
@@ -2,8 +2,30 @@ import React, {Component} from 'react';
 import NewSingle from './NewSingle';
 
 class News extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+        news:[],
+    }
+}
+
+componentDidMount(){
+    const url = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-24&sortBy=publishedAt&apiKey=0971b0705e864f828690bc27831d3770";
+
+    fetch(url)
+     .then((response)=> {
+         return response.json();
+     })
+     .then((data) => {
+         this.setState({
+             news: data.articles
+         })
+     })
+     .catch((error) => console.log(error));
+}
+
     renderItems() {
-        return this.props.items.map((item) => (
+        return this.state.news.map((item) => (
             <NewSingle key={item.id} item={item} />
         ));
     }
