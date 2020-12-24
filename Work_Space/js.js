7719dfdde28bbc0d861b8dd3886a143277cb87c5
Diab <emailcodydiab@gmail.com>
2020-12-24 07:16:41 -0600

    sort/count hidden test failed

--- Work_Space/js.js
+++ Work_Space/js.js
@@ -4,3 +4,22 @@ function calculatePercentage(num,per){
     {return(num/100)*per;}
 }
 //
+
+// sort and count alg
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let num = candles.length;
+    let values = candles.slice();
+    let sortedValues = values.sort();
+    let max = sortedValues[num-1]
+    var count = 0;
+    
+    for(let i=0;i<sortedValues.length;i++){
+        if(sortedValues[i]==max){
+            count++
+     }
+    }
+    return(count)
+
+}
