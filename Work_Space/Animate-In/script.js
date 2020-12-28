0eecff1372aaee3353860772a72580bea36b0009
Diab <emailcodydiab@gmail.com>
2020-12-28 08:26:20 -0600

    modal timeout example

--- Work_Space/Animate-In/script.js
+++ Work_Space/Animate-In/script.js
@@ -3,3 +3,35 @@
 //         document.getElementById('animate').style.top = '0px';
 //     }, 2000);
 // }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function timeOut() {
+    setTimeout(function(){
+       modal.style.display = "none"
+    },1000);
+}
+btn.onclick = function() {
+   modal.style.display = "block";
+   timeOut()
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
